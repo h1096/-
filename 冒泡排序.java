84d4209bc245d22c1e98767e9b7514ca079377f0
h1096 <72010697+h1096@users.noreply.github.com>
2022-04-28 15:30:59 +0800

    Create 冒泡排序.java

---
+++ 冒泡排序.java
@@ -0,0 +1,12 @@
+public class BubbleSort {
+    public void sort(Comparable[] objects, boolean asc) {
+        for (int i = 0; i < objects.length - 1; i++) {
+            for (int j = 0; j < objects.length - i - 1; j++) {
+                // 为啥要取反? 只有不满足时才交换
+                if (!compare(objects[j], objects[j + 1], asc)) {
+                    swap(objects, j, j + 1);
+                }
+            }
+        }
+    }
+}
